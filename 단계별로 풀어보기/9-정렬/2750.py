d5adc8f87715b93dbadc413bc348d1ad4f5e6daf
jichan <ah3851wen@naver.com>
2022-10-05 18:22:48 +0900

    백준

--- 단계별로 풀어보기/9-정렬/2750.py
+++ 단계별로 풀어보기/9-정렬/2750.py
@@ -8,11 +8,47 @@ for i in a:
     print(i)
 
 
-N = int(input())
-M = set()
-for i in range(N) :
-	M.add(int(input()) #append대신에 add사용
-M = list(M)
-M.sort()
-for i in range(len(M)) : 
-	print(M[i])    
+# N = int(input())
+# M = set()
+# for i in range(N) :
+# 	M.add(int(input()) #append대신에 add사용
+# M = list(M)
+# M.sort()
+# for i in range(len(M)) : 
+# 	print(M[i])    
+
+
+# 버블 정렬
+# N = int(input())
+
+# M = []
+
+# for i in range(N) : 
+#     M.append(int(input()))
+
+# # Bubble Sort
+# for i in range(len(M)) : 
+#     for j in range(len(M)) : 
+#         if M[i] < M[j] : 
+#             M[i], M[j] = M[j], M[i]
+            
+# for n in M : 
+#     print(n)    
+
+
+# 삽입 정렬
+#N = int(input())
+#M = []
+#
+#for i in range(N) : 
+#    M.append(int(input()))
+#
+## Insert Sort
+#for i in range(1, len(M)) :
+#    while (i>0) & (M[i] < M[i-1]) :
+#        M[i], M[i-1] = M[i-1], M[i]
+#        
+#        i -= 1
+#        
+#for n in M : 
+#    print(n)
