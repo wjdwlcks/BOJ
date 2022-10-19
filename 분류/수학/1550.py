9d864b5be18b98b1c9ea362ec2cac2edb76a05dd
jichan <ah3851wen@naver.com>
2022-10-19 20:36:35 +0900

    백준

---
+++ 분류/수학/1550.py
@@ -0,0 +1,12 @@
+print(int(input(),16))
+
+#import sys
+#input = sys.stdin.readline
+#st = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+#s = input().strip()
+#result = 0
+#s = s[::-1]
+#for i in range(1, len(s)):
+#    result += st.index(s[i]) * (16 ** i)
+#result += st.index(s[0])
+#print(result)
