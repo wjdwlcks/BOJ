4a9c5b7d2a2f5bb3f2a279de9c77c154607eea6d
jichan <ah3851wen@naver.com>
2022-10-21 20:09:12 +0900

    백준

--- solved.ac/CLASS2/1920.py
+++ solved.ac/CLASS2/1920.py
@@ -1,13 +1,25 @@
 import sys
-input()
+sys.stdin.readline()
 A= list(map(int,sys.stdin.readline().split()))
-input()
+sys.stdin.readline()
 B= list(map(int,sys.stdin.readline().split()))
 
+set(A)
 A.sort()
 
 for i in  B:
-    if i in A:
-        print(1)
-    else: 
-        print(0)
+    mid = len(A)//2 
+    while 1 :
+        
+        if (mid == 0 or mid== len(A)-1) and i != A[mid]:
+            sys.stdout.write('0\n')
+            break   
+
+        if i < A[mid]:     
+            mid = mid//2
+        elif i > A[mid]:       
+            mid = (len(A)+mid)//2
+        else:
+            sys.stdout.write('1\n')
+            break
+    sys.stdout.write('0\n')   
