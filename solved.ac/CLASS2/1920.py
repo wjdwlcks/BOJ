362c6033a5a8f75ae12f513da1c0d5063b22d599
wjdwlcks <ah3851wen@naver.com>
2022-10-22 22:22:01 +0900

    백준

--- solved.ac/CLASS2/1920.py
+++ solved.ac/CLASS2/1920.py
@@ -1,4 +1,8 @@
 import sys
+import math
+def roundUp(num):
+    return int(num) + 1 if (num - int(num)) >= 0.5 else int(num)
+
 sys.stdin.readline()
 A= list(map(int,sys.stdin.readline().split()))
 sys.stdin.readline()
@@ -8,18 +12,17 @@ set(A)
 A.sort()
 
 for i in  B:
-    mid = len(A)//2 
+    mid = roundUp(len(A)/2 )
     while 1 :
-        
-        if (mid == 0 or mid== len(A)-1) and i != A[mid]:
+        if (mid == 0 or mid >= len(A)) and i != A[mid]:
             sys.stdout.write('0\n')
-            break   
+            break                 
 
         if i < A[mid]:     
             mid = mid//2
         elif i > A[mid]:       
-            mid = (len(A)+mid)//2
+            mid = mid + roundUp(mid/2)
         else:
             sys.stdout.write('1\n')
-            break
-    sys.stdout.write('0\n')   
+            break 
+ 
