ef1e7b47912effa192c2eca734cab7c44f483354
wjdwlcks <ah3851wen@naver.com>
2022-10-23 23:13:22 +0900

    백준

--- solved.ac/CLASS2/1920.py
+++ solved.ac/CLASS2/1920.py
@@ -1,8 +1,4 @@
 import sys
-import math
-def roundUp(num):
-    return int(num) + 1 if (num - int(num)) >= 0.5 else int(num)
-
 sys.stdin.readline()
 A= list(map(int,sys.stdin.readline().split()))
 sys.stdin.readline()
@@ -11,18 +7,33 @@ B= list(map(int,sys.stdin.readline().split()))
 set(A)
 A.sort()
 
-for i in  B:
-    mid = roundUp(len(A)/2 )
-    while 1 :
-        if (mid == 0 or mid >= len(A)) and i != A[mid]:
-            sys.stdout.write('0\n')
-            break                 
+for i in  B:        
+    left = 0
+    right = len(A)-1
+    while left <= right:            
+        mid = (left+right)//2   
+        if i==A[mid]:
+           sys.stdout.write('1\n')
+           break
+        elif i < A[mid]:     
+            right = mid-1
+        else: 
+             left = mid+1                    
+    else:    
+        sys.stdout.write('0\n')
 
-        if i < A[mid]:     
-            mid = mid//2
-        elif i > A[mid]:       
-            mid = mid + roundUp(mid/2)
-        else:
-            sys.stdout.write('1\n')
-            break 
- 
+
+
+## 입력
+#N = int(input())
+#A = set(map(int, input().split()))	# 탐색 시간을 줄이기 위해 set으로 받음
+#M = int(input())
+#arr = list(map(int, input().split()))
+#
+#for num in arr:				# arr의 각 원소별로 탐색
+#    print(1) if num in A else print(0)	# num이 A 안에 있으면 1, 없으면 0 출력        
+    
+        
+                  
+      
+
